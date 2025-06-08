6f7ecd4cf3730e0e1a559f0675f82a8bcc15ab88
Masashi Shiomi <bkmn.31519@gmail.com>
2025-06-08 21:39:25 +0900

    画面選択時にモーダルを表示

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,6 @@
 import Button from "@/components/Button";
+import CircleButton from "@/components/CircleButton";
+import IconButton from "@/components/IconButton";
 import ImageViewer from "@/components/ImageViewer";
 import * as ImagePicker from 'expo-image-picker';
 import { useState } from "react";
@@ -8,6 +10,8 @@ const PlaceholderImage = require('../../assets/images/background-image.png');
 
 export default function Index() {
   const [selectedImg, setSelectedImg] = useState<string | undefined>(undefined);
+  // モーダルを開くボタンやその他のオプション表示/非表示を設定
+  const [showAppOptions, setShowAppOptions] = useState(false);
   const pickImageAsync = async () => {
     // launchImageLibraryAsync：デバイスの画像ライブラリを開くメソッド
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -20,20 +24,45 @@ export default function Index() {
     // 画像を選択できた時
     if (!result.canceled) {
       setSelectedImg(result.assets[0].uri);
+      setShowAppOptions(true);
     } else {
       alert('You did not select any image.');
     }
   }
+
+  const onReset = () => {
+    setShowAppOptions(false);
+  };
+
+  const onAddSticker = () => {
+    // we will implement this later
+  };
+
+  const onSaveImageAsync = async () => {
+    // we will implement this later
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer imgSource={PlaceholderImage} selectedImg={selectedImg} />
       </View>
-      <View style={styles.footerContainer}>
+      {/* 画像を選択したらモーダルを表示させる */}
+      {showAppOptions ? (
+        <View style={styles.optionsContainer}>
+          <View style={styles.optionsRow}>
+            <IconButton icon="refresh" label="Reset" onPress={onReset} />
+            <CircleButton onPress={onAddSticker} />
+            <IconButton icon="save-alt" label="Save" onPress={onSaveImageAsync} />
+          </View>
+        </View>
+      ) : (
+        <View style={styles.footerContainer}>
         {/* ボタンを押すとlaunchImageLibraryAsync関数を実行して画像選択する。 */}
         <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
-        <Button label="Use this photo" />
+        <Button label="Use this photo" onPress={() => setShowAppOptions(true)}/>
       </View>
+      )}
     </View>
   );
 }
@@ -50,5 +79,13 @@ const styles = StyleSheet.create({
   footerContainer: {
     flex: 1 / 3,
     alignItems: 'center',
+  },
+  optionsContainer: {
+    position: 'absolute',
+    bottom: 80,
+  },
+  optionsRow: {
+    alignItems: 'center',
+    flexDirection: 'row',
   },
 });
