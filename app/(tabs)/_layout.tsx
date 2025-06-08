42de4c22cb728d0d5e18867f271dd96236697fc5
Masashi Shiomi <bkmn.31519@gmail.com>
2025-06-08 17:17:29 +0900

    ナビゲーションを実装

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,11 @@
+import { Stack } from "expo-router";
+
+export default function TabLayout() {
+  return (
+    <Stack>
+      {/* Stackとはアプリ内の画面間を移動するための基盤 */}
+      <Stack.Screen name="index" options={{ title: "Home" }} />
+      <Stack.Screen name="about" options={{ title: "About" }} />
+    </Stack>
+  );
+}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,8 +4,7 @@ export default function RootLayout() {
   return (
     <Stack>
       {/* Stackとはアプリ内の画面間を移動するための基盤 */}
-      <Stack.Screen name="index" options={{ title: "Home" }} />
-      <Stack.Screen name="about" options={{ title: "About" }} />
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
     </Stack>
   );
 }
